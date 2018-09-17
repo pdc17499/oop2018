7aa97ca03b292de61addc2f8b5df37b965a67c15
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-09-17 21:03:28 +0700

    add Student

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,14 +3,50 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String id;
+    private String name;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
 
     /**
      * Constructor 1
      */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group=group;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getEmail(){
+        return email;
+    }
     Student() {
-        // TODO:
+
+        name ="Student";
+        id="000";
+        group="INT22041";
+        email="uet@vnu.edu.vn";
     }
 
     /**
@@ -20,7 +56,11 @@ public class Student {
      * @param em
      */
     Student(String n, String sid, String em) {
-        // TODO:
+        this.name=n;
+        this.id=sid;
+        this.email=em;
+        this.group="INT22041";
+
     }
 
     /**
@@ -28,11 +68,18 @@ public class Student {
      * @param s
      */
     Student(Student s) {
-        // TODO:
+        this.name=s.name;
+        this.id=s.id;
+        this.email=s.email;
+        this.group=s.group;
+
     }
 
     String getInfo() {
-        // TODO:
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(group);
+        System.out.println(email);
         return null; // xóa dòng này sau khi cài đặt
     }
 }
