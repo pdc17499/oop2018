52dcccedd49935dcf051f08d3093308fc6eb3693
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-09-19 11:38:53 +0700

    add StudentManagement 02

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,21 +3,97 @@ package week1;
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    int max=100;
+       Student[] students=new Student[max];
 
     public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+      return (s1.getGroup().equals(s2.getGroup()));
+
     }
 
     void studentsByGroup() {
-        // TODO:
+
+        for (int i=0;i<max-1;i++)
+            for (int j=1;j<max;j++)
+                if (students[i].getGroup().compareTo(students[j].getGroup())<0) {
+                    Student A = new Student();
+                    A = students[i];
+                    students[i] = students[j];
+                    students[j] = A;
+                }
+
+        String B = " ";
+         int i=0;
+
+        while (i<max) {
+            if (students[i].getGroup().equals(B) == false) {
+                B = students[i].getGroup();
+                System.out.println("Cac sinh vien thuoc lop " + B + "la: ");
+                System.out.println(students[i]);
+            }
+            else {
+                System.out.println(students[i]);
+            }
+            i++;
+        }
     }
 
+
     void removeStudent(String id) {
-        // TODO:
+
+        for( int i = 0 ; i < 100 ; i ++ )
+        {
+            int sinhvienthuN;
+            if (students[i].getId() == id )
+            {
+                sinhvienthuN = i;
+                for(int j = i ; j < 100 ; j++)
+                {
+                    students[j] = students[j+1];
+                }
+                max = max - 1;
+
+            }
+        }
     }
 
     public static void main(String[] args) {
-        // TODO:
+
+        Student a=new Student();
+        a.setName("Chu Duc Phuc");
+        a.setEmail("vituonglaiconem@gmail.com");
+        a.setGroup("INT2204");
+        a.setId("17020957");
+
+        System.out.println("ten Sv:" + a.getName());
+        a.getInfo();
+
+        //lam cau 8:
+        Student b=new Student();
+        b.getInfo();
+
+        Student c=new Student("Phuc ", "17020957", "abcxyz@gmail.com");
+            c.getInfo();
+
+        Student d=new Student(a);
+            d.getInfo();
+
+        // cau10
+        Student s01 = new Student("A", "17020711", "num1@gmail.com");
+
+        Student s02 = new Student("B", "17020721", "num2@gmail.com");
+
+        Student s03 = new Student();
+         s03.setGroup("INT22042");
+        StudentManagement ss = new StudentManagement();
+        System.out.println( "Student 1vs2 " +ss.sameGroup(s01, s02));
+        System.out.println("Student 1vs3 " + ss.sameGroup(s01, s03));
+        System.out.println("Student 2vs3 " + ss.sameGroup(s02, s03));
+
+
+
+
+
+
     }
 }
