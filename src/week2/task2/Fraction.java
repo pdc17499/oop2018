8516db6f6e7aacd245a7d084243abc054eaee548
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-09-26 00:12:23 +0700

    add task2

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,30 +1,71 @@
 package week2.task2;
 
+import  week2.task1.Task1;
+
+import javax.xml.transform.sax.SAXSource;
+
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
+    private int numerator, denominator;
+    public String Infor(){
+        return numerator+"/"+denominator;
+    }
 
     public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+    }
+
+    // phuong thuc rut gon phan so thanh toi gian
+    public Fraction minimals() {
+        int ucln = Task1.gcd(this.numerator, this.denominator);
+        return new Fraction(this.numerator / ucln, this.denominator / ucln);
     }
 
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        if (this.denominator == other.denominator)
+            return new Fraction(this.numerator + other.numerator, other.denominator).minimals();
+        else {
+            return new Fraction(this.numerator * other.denominator + this.denominator * other.numerator, this.denominator * other.denominator).minimals();
+        }
+
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        if (this.denominator == other.denominator)
+            return new Fraction(this.numerator - other.numerator, other.denominator).minimals();
+        else {
+            return new Fraction(this.numerator * other.denominator - this.denominator * other.numerator, this.denominator * other.denominator).minimals();
+        }
+
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator).minimals();
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        return new Fraction(this.numerator * other.denominator, this.denominator * other.numerator).minimals();
+    }
+
+    public boolean equals(Fraction other) {
+        if (this.subtract(other).numerator == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Fraction a = new Fraction(12, 5);
+        Fraction b = new Fraction(18, 7);
+
+        System.out.println(a.divide(b).Infor());
+
     }
 }
