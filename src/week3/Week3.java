84e4cfb4d9d7cfb33d011259adf7c971de107045
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-10-03 00:54:38 +0700

    1.1

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -3,13 +3,17 @@ package week3;
 public class Week3 {
 
     public static int max(int m, int n) {
-        // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if(m>=n) return m;
+        else return n;
     }
 
     public static int minOfArray(int[] array) {
-        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int min=array[0];
+        for(int i=0;i<array.length;i++){
+            if(min>array[i]) min=array[i];
+        }
+        return min;
+
     }
 
     /**
@@ -20,6 +24,16 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI= (weight/(height*height));
+        if(BMI<18.5) return "Thieu can";
+       if(BMI>=18.5&&BMI<=22.99) return "Binh thuong";
+       if(BMI>=23&&BMI<=24.99) return "Thua can";
+       else return "Beo phi";
+
     }
+    public static void main(String[] args){
+
+        System.out.println(calculateBMI(50,1.70));
+    }
+
 }
