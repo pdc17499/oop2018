440e2032aa78627ec5e6e03c7e251ce02e97c3ea
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-10-17 09:14:13 +0700

    00

---
+++ src/week5_6/Circle.java
@@ -0,0 +1,41 @@
+package week5_6;
+
+
+public class Circle extends Shape {
+    final double PI = 3.14;
+    private double radius = 1.0;
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    //tinh dien tich
+    public double getArea() {
+        return PI * getRadius() * getRadius();
+
+
+    }
+    //tinh chu vi
+    public double getPerimeter() {
+        return 2 * PI * getRadius();
+
+    }
+}
---
+++ src/week5_6/Rectangle.java
@@ -0,0 +1,47 @@
+package week5_6;
+
+public class Rectangle extends Shape {
+    private double width;
+    private double length;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(double width, double length) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return getLength() * getWidth();
+
+    }
+
+    public double getPerimeter() {
+        return 2 * (getWidth() + getLength());
+
+    }
+}
