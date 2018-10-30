c4d3370d517c447a41ecbc041e22154ee6487555
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-10-30 23:02:32 +0700

    1

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,4 +1,10 @@
 package week7.task1;
 
-public class Addition {
+public class Addition extends  BinaryExpression{
+    Expression left;
+    Expression right;
+    public  Addition(Expression a,Expression b){
+        left=a;
+        right=b;
+    }
 }
