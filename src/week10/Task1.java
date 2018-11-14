74aa6b7f1d66e9319dcfae51bf847a23dc995473
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-11-14 10:55:04 +0700

    add

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,36 @@
 package week10;
 
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+
+    public List<String> getAllFunctions(String path) {
+        List<String> myList = new ArrayList<> ();
+        try {
+            FileInputStream fin = new FileInputStream (path);
+            int i = 0;
+            String str = "";
+            while ((i = fin.read ()) != -1) {
+                str += (char) i;
+            }
+            while (str.indexOf ("public static") != -1) {
+                int start = str.indexOf ("public static");
+                int end = str.indexOf ("//end");
+                String s = str.substring (start, end);
+                str = str.substring (end+ 1);
+                myList.add (s);
+            }
+            return myList;
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+        return null;
+    }
+
+
+    public static void main(String Args[]){
+
+    }
 }
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,27 @@
 package week10;
-
 public class Task2 {
+
+    public void bubbleSort(double[] temp){
+        for(int i=0;i<temp.length-1;i++){
+            for(int j=0;j<temp.length-1;j++){
+                if(temp[j]>temp[j+1]){
+                    double tmp = temp[j];
+                    temp[j]= temp[j+1];
+                    temp[j+1]= tmp;
+                }
+            }
+        }
+    }
+    public static void main(String args[]){
+
+        double[] ar=new double[1000];
+        for(int i=0;i<1000;i++){
+            ar[i] = Math.random()*(1000-1) +1;
+        }
+        Task2 s= new Task2();
+        s.bubbleSort(ar);
+        for(int i=0;i<ar.length;i++){
+            System.out.println(ar[i]);
+        }
+    }
 }
