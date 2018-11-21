95f22a9ecf345bbc461045dfb8d1fae091884793
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-11-21 13:02:37 +0700

    add

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,27 @@
 package week11;
-
 public class Task1 {
+    public <T extends Comparable<T>> void Sort(T[] Arr){
+        for(int i=0;i<Arr.length-1;i++){
+            for(int j=i+1;j<Arr.length;j++){
+                if(Arr[i].compareTo(Arr[j])>0){
+                    T t = Arr[j];
+                    Arr[j]= Arr[i];
+                    Arr[i] = t;
+                }
+            }
+        }
+    }
+    public <T>void printArray(T[] Arr){
+        for(int i=0;i<Arr.length;i++){
+            System.out.print(Arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+
+        Double A[] = {1.5,7.3,6.7,0.2,12.1};
+        Task1 task1 = new Task1();
+        task1.<Double>Sort(A);
+        task1.<Double>printArray(A);
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,24 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public <T extends Comparable<T>> T MaxArray(ArrayList<T> Arr){
+        T Max = Arr.get(0);
+        for(int i=0;i<Arr.size();i++){
+            if(Arr.get(i).compareTo(Max)>0){
+                Max = Arr.get(i);
+            }
+        }
+        return Max;
+    }
+    public static void main(String Args[]){
+        ArrayList<Double> A = new ArrayList<Double>();
+        A.add(2.3);
+        A.add(0.6);
+        A.add(7.8);
+        Task2 task2 = new Task2();
+        System.out.println(task2.MaxArray (A));
+    }
+
 }
